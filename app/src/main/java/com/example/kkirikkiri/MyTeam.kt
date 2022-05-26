4c3080b0e0a2d9d8c9369c8286b4fc5c53d4eca6
황지민 <80872558+krapoi@users.noreply.github.com>
2022-05-26 21:02:11 +0900

    🎈::최초커밋

---
+++ app/src/main/java/com/example/kkirikkiri/MyTeam.kt
@@ -0,0 +1,11 @@
+package com.example.kkirikkiri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyTeam : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_team)
+    }
+}
