70ae99fe2f3c62e0a4d1c560cbbf339eb0777cbe
황지민 <80872558+krapoi@users.noreply.github.com>
2022-06-16 17:30:51 +0900

    🖊:: pid 액티비티 생성

---
+++ app/src/main/java/com/example/kkirikkiri/Pid.kt
@@ -0,0 +1,11 @@
+package com.example.kkirikkiri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pid : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pid)
+    }
+}
